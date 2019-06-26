cfcb60b9fbd4ffee1ad0fe00aacd289cf7f49fbe
Alex Devero <deveroalex@gmail.com>
2019-06-26 11:30:22 +0200

    Add constants

---
+++ constants/styles.ts
@@ -0,0 +1,16 @@
+import Colors from './colors'
+
+export default {
+  headerStyle: {
+    boxShadow: Colors.boxShadow
+  },
+  headerTitleStyle: {
+    fontSize: '15px'
+  },
+  viewContainerStyle: {
+    flex: 1,
+    paddingTop: 15,
+    paddingHorizontal: 15,
+    backgroundColor: '#fff'
+  }
+}
